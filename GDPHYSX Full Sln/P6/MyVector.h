9582318c6b6c49e4e75932a4de783ffaf007914b
Paul Macaraeg <paul_macaraeg@dlsu.edu.ph>
2024-05-14 20:54:47 +0800

    For Assignment 2 uwu by Paul Macaraeg
    For Assignment 2 uwu by Paul Macaraeg

--- GDPHYSX Full Sln/P6/MyVector.h
+++ GDPHYSX Full Sln/P6/MyVector.h
@@ -10,20 +10,22 @@ namespace P6 {
 		public:
 			float x, y, z;
 
-			MyVector() : x(0), y(0), z(0) {}
-			MyVector(const float _x, const float _y, const float _z) : x(_x), y(_y), z(_z) {}
+			MyVector() : x(0),y(0),z(0) {}
+			MyVector(const float _x, const float _y, const float _z);
 
-			explicit operator glm::vec3() const 
-			{ 
-				return glm::vec3(x, y, z); 
-			}
+			explicit operator glm::vec3() const;
+			
 
-			void operator+= (const MyVector v) 
-			{
-				this->x += v.x;
-				this->y += v.y;
-				this->z += v.z;
-			}
+			float Magnitude();
+			MyVector Direction();
+			void operator+=(const MyVector v); // addition
+			void operator-=(const MyVector v); // subtraction
+			MyVector scalarMultiplication(float ScaleValue);
+			void operator*=(const MyVector v); // Component Product
+			float dotProduct(const MyVector v);
+			MyVector vectorProduct(const MyVector v);
 
+			
+			
 	};
 }
